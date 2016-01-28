a5886f328c5f4a44925a9d389aec81a308dfd97f
MenushkaSWS <menushka@savewithsprout.com>
2016-01-27 22:51:49 -0500

    Update JS

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+var update = function(){
+	$(".animejs").each(function(i){
+		var classList = $(this).attr('class').split(/\s+/);
+		classList[1] = $("#inputField").val();
+
+		$(this).attr('class', classList.toString().replace(/,/g, " "));
+	});
+
+	updateAnimeJS();
+}
+
 $(document).ready(function(){
 	$("#inputButton").click(function(){
-		$(".animejs").each(function(i){
-			var classList = $(this).attr('class').split(/\s+/);
-			classList[1] = $("#inputField").val();
-
-			$(this).attr('class', classList.toString().replace(/,/g, " "));
-		});
-
-		updateAnimeJS();
+		update();
 	});
+
+	$("#inputField").keyup(function (e) {
+	    if (e.keyCode == 13) {
+	        update();
+	    }
+	});
+
 });
